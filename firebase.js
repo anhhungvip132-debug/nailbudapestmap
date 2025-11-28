ac38fafb44ad0f8f9c6612a8c79957f2f3243b9c
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-11-29 00:43:01 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkq0DKue8884V3AAu_O-cpEmlcalJhDOs",
+  authDomain: "nailfinder-6146a.firebaseapp.com",
+  projectId: "nailfinder-6146a",
+  storageBucket: "nailfinder-6146a.firebasestorage.app",
+  messagingSenderId: "703195233020",
+  appId: "1:703195233020:web:d0fd8877b2986f03a27579",
+  measurementId: "G-DSW8H0CZN3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
