bf397a02069752a74682d5c1484f8dc203845035
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-11-29 01:40:00 +0100

    Fix syntax error in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
@@ -10,4 +10,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
