1b747ead230eba4644c62e2a3736d1b2d93467a7
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-11-29 01:30:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: './index.html',
-        admin: './src/admin/login.html',
-        dashboard: './src/admin/dashboard.html'
+        admin: './admin/login.html',
+        dashboard: './admin/dashboard.html'
       }
     }
   }
-})
+});
