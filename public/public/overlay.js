4b43a2c78dcc8e59fa24e36c7c879ae107152358
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-11-29 14:52:15 +0100

    Delete public/public directory

--- public/public/overlay.js
+++
@@ -1,8 +0,0 @@
-export function openOverlay(html) {
-  const o = document.getElementById("overlay");
-  o.innerHTML = html;
-  o.classList.add("active");
-}
-export function closeOverlay() {
-  document.getElementById("overlay").classList.remove("active");
-}
