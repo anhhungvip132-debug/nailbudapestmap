cd0a31811fd2b1877930628b04531fd664f84cb0
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-11-29 01:01:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  root: './src',
   build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        admin: './src/admin/login.html'
+      }
+    }
   }
 })
