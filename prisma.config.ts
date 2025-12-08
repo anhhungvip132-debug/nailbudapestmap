961608ec316855afdfd7a132e93c556549966e2d
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-12-08 02:02:52 +0100

    Fix Prisma 7 schema (removed datasource url)

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from "@prisma/config";
 export default defineConfig({
   datasource: {
     db: {
-      // Sử dụng DATABASE_URL từ biến môi trường
       url: process.env.DATABASE_URL!,
     },
   },
-  // Prisma Client config
-  client: {
-    // Không cần cấu hình gì thêm
-  },
+  client: {},
 });
