a06cea1adce18472faa8a8dcf2db89bbdfeb7caa
anhhungvip132-debug <anhhungvip132@gmail.com>
2025-12-11 01:01:03 +0100

    Restore full UI page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Hero from "@/components/ui/Hero";
 import FeaturedSalons from "@/components/ui/FeaturedSalons";
 import NearestSalons from "@/components/ui/NearestSalons";
@@ -6,12 +8,29 @@ import OwnerSection from "@/components/ui/OwnerSection";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col gap-20 pb-20">
+      {/* Banner chính */}
       <Hero />
-      <FeaturedSalons />
-      <NearestSalons />
-      <BlogSection />
-      <OwnerSection />
+
+      {/* Salon Nổi Bật */}
+      <section id="featured-salons" className="scroll-mt-20">
+        <FeaturedSalons />
+      </section>
+
+      {/* Salon Gần Nhất */}
+      <section id="nearest-salons" className="scroll-mt-20">
+        <NearestSalons />
+      </section>
+
+      {/* Blog - Bài viết mới */}
+      <section id="blogs" className="scroll-mt-20">
+        <BlogSection />
+      </section>
+
+      {/* Người sáng lập */}
+      <section id="owner" className="scroll-mt-20">
+        <OwnerSection />
+      </section>
     </main>
   );
 }
